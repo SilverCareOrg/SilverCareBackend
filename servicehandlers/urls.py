0fcca52a998765806496f40340bc103a76ddb6f9
Sorin-Ioan-Alexandru Bîrchi <84462350+SorinAlexB@users.noreply.github.com>
2023-10-12 13:10:32 -0900

    Delete servicehandlers directory

--- servicehandlers/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import get_services, get_services_by_organiser, delete_service
-
-urlpatterns = [
-    path('get_services/', get_services, name = 'get_services'),
-    path('get_services_by_organiser/', get_services_by_organiser, name = 'get_services_by_organiser'),
-    path('delete_service/', delete_service, name = 'delete_service')
-]
