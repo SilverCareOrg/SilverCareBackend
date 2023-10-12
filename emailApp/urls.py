ae76eb26f06b4875b78cb53492d90997be7febc9
Sorin-Ioan-Alexandru Bîrchi <84462350+SorinAlexB@users.noreply.github.com>
2023-10-12 13:09:32 -0900

    Delete emailApp directory

--- emailApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import checkout_send_email,\
-    guest_checkout_send_email
-
-urlpatterns = [
-    # path('checkout_send_email', checkout_send_email),
-    # path('guest_checkout_send_email', guest_checkout_send_email),
-]
