2b747065a962e36c8fd425d8207b94c783e290fb
Sorin-Ioan-Alexandru Bîrchi <84462350+SorinAlexB@users.noreply.github.com>
2023-10-13 02:17:25 +0300

    Add files via upload

---
+++ silvercare/userhandlers/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import get_users, get_users_by_email, set_staff
+
+urlpatterns = [
+    path('get_users/', get_users, name = 'get_users'),
+    path('get_users_by_email/', get_users_by_email, name = 'get_users_by_email'),
+    path('set_staff', set_staff, name = 'set_staff')
+]
--- silvercare/userhandlers/views.py
+++ silvercare/userhandlers/views.py
@@ -1,3 +1,69 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User
+from django.http import JsonResponse
+from rest_framework.response import Response
+from login.utils import get_user_from_token_request
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def get_users(request):
+    try:
+        user = get_user_from_token_request(request)
+        if not user.is_staff:
+            return Response({'message': 'You are not authorized to get users'}, status=403)
+        inf_lim = request.GET.get('inf_lim', 0)
+        sup_lim = request.GET.get('sup_lim', 20)
+        if inf_lim > sup_lim:
+            inf_lim, sup_lim = sup_lim, inf_lim
+        users = User.objects.filter(pk__gte = inf_lim, pk__lte = sup_lim)
+        data = list(users.values())
+        return JsonResponse(data, safe = False)
+    except Exception as e:
+        return Response({'message': 'An error occurred.'}, status=500)
+
+
+
+@api_view(['GET'])
+def get_users_by_email(request):
+    try:
+        user = get_user_from_token_request(request) 
+        if not user.is_staff:
+            return Response({'message': 'You are not authorized to get users'}, status=403)
+
+        search_email = request.GET.get('search_email', '') 
+        users = User.objects.filter(email__contains=search_email)
+        
+        data = list(users.values())
+
+        return JsonResponse(data, safe=False)
+    except Exception as e:
+        return Response({'message': 'An error occurred.'}, status=500)
+    
+
+@api_view(['POST'])
+def set_staff(request):
+    try:
+        user = get_user_from_token_request(request) 
+        if not user.is_staff: 
+            return Response({'message': 'You are not authorized to set staff'}, status=403)
+        staff_email = request.GET.get('staff_email', '')
+        users = User.objects.filter(email__contains = staff_email)
+        users.is_staff = True
+        return Response({'message':'Staff set successfully'})
+    except Exception as e:
+        return Response({'message': 'An error occurred.'}, status=500)
+        
+    
+
+
+
+    # @api_view(['GET'])
+# def get_users_by_email(request):
+#     user = get_user_from_token_request(request)
+#     if not user.is_staff:
+#         return Response({'message': 'You are not authorized to add services'}, status=403)
+#     search_email = request.GET.get('search_email', 0)
+#     users = User.objects.filter(email__contains = search_email)
+#     data = list(users.values())
+#     return JsonResponse(data, safe = False)
