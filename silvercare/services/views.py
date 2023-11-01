86a66ae021a131a27cdc7326acdfad43e6e27edb
avramstefan <94166855+avramstefan@users.noreply.github.com>
2023-11-01 14:11:34 +0200

    Added workflow for adding service

--- silvercare/services/views.py
+++ silvercare/services/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
-from services.models import Service
+from services.models import Service, ServiceOption, MapLocation
 import json
+from django.utils.dateparse import parse_duration
+from django.utils.timezone import timedelta
 from django.http import HttpResponse, JsonResponse
 from login.utils import get_user_from_token_request
 from rest_framework import serializers
@@ -14,6 +16,9 @@ import os
 import base64
 import random as rd
 import environ
+from unidecode import unidecode
+import uuid
+from datetime import datetime
 
 env = environ.Env()
 environ.Env.read_env()
@@ -42,37 +47,108 @@ class CreateServiceView(APIView):
         if not user.is_staff:
             return Response({'message': 'You are not authorized to add services'}, status=400)
 
-        if serializer.is_valid():            
-            name = serializer.validated_data.get('name')
-            category = serializer.validated_data.get('category').lower()
-            price = serializer.validated_data.get('price')
-            description = serializer.validated_data.get('description')
-            organiser = serializer.validated_data.get('organiser')
+        data = request.data
+        service_name = data.get('name')
+        service_raw_name = unidecode(service_name)
+        service_description = data.get('description')
+        service_options_common_city = True if data.get('options_common_city') == "true" else False
+        service_common_location = True if data.get('common_location') == "true" else False
+        service_category = data.get('category')
+        service_iban = data.get('iban')
+        
+        options = data.get("options")
+        sections = data.get("sections")
+        
+        file = request.FILES.get('image')
+        image_type = str(file).split('.')[-1]
 
-            file = request.FILES.get('file')
-            image_type = str(file).split('.')[-1]
+        # generate_file_name
+        file_name = str(uuid.uuid4())
 
-            if "/var/www" in PATH_TO_FIMG:
-                f = open(PATH_TO_FIMG + str(file), 'wb')
-            else:
-                f = open(os.path.join(os.path.dirname(__file__), 'images/' + str(file)), 'wb')
-
-            f.write(file.read())
-            f.close()
-            
-            service = Service.objects.create(name=name,
-                                             category=category,
-                                             price=price,
-                                             description=description,
-                                             image=str(file),
-                                             organiser=organiser,
-                                             image_type=image_type)
-            service.save()
-
-            return Response({'message': 'Service created successfully'})
+        if "/var/www" in PATH_TO_FIMG:
+            f = open(PATH_TO_FIMG + file_name + "." + image_type, 'wb')
         else:
-            print(serializer.errors)
-            return Response(serializer.errors, status=400)
+            f = open(os.path.join(os.path.dirname(__file__), 'images/' + file_name + "." + image_type), 'wb')
+
+        f.write(file.read())
+        f.close()
+        
+        service_obj = Service.objects.create(name = service_name,
+                                            raw_name = service_raw_name,
+                                            description = service_description,
+                                            options_common_city = service_options_common_city,
+                                            common_location = service_common_location,
+                                            category = service_category,
+                                            iban = service_iban,
+                                            image = file_name,
+                                            image_type = image_type)
+        
+        if service_options_common_city:
+            service_city = data.get('city')
+            service_obj.city = service_city
+            
+            service_county = data.get('county')
+            service_obj.county = service_county
+            
+        if service_common_location:
+            service_location = data.get('location')
+            service_obj.location = service_location
+            
+            service_map_location = data.get('map_location')
+            if service_map_location != "":
+                service_map_location = service_map_location.split(",")
+                latitude = float(service_map_location[0])
+                longitude = float(service_map_location[1][1:])
+                map_location_obj = MapLocation.objects.create(latitude = latitude,
+                                                                longitude = longitude)
+                service_obj.map_location = map_location_obj
+        
+        options = json.loads(options)
+        
+        for option in options:
+            option_name = option.get("name")
+            option_price = option.get("price")
+            option_duration = parse_duration(option.get("duration"))
+            option_date = option.get("date")
+            option_location = option.get("location")
+            option_map_location = option.get("map_location")
+            option_details = option.get("details")
+            option_city = option.get("city")
+            option_county = option.get("county")
+            
+            
+            option_obj = ServiceOption.objects.create(name = option_name,
+                                                      price = option_price,
+                                                      duration = option_duration,
+                                                      location = option_location,
+                                                      details = option_details,
+                                                      city = option_city,
+                                                      county = option_county,
+                                                      service = service_obj)
+            
+            try:
+                option_date = option.get("date_time")
+                
+                if option_date != "":
+                    option_date = datetime.strptime(option_date, "%Y-%m-%dT%H:%M").date()
+                    option_obj.date = option_date
+            except:
+                pass
+            
+            if option_map_location != "":
+                option_map_location = option_map_location.split(",")
+                latitude = float(option_map_location[0])
+                longitude = float(option_map_location[1][1:])
+                map_location_obj = MapLocation.objects.create(latitude = latitude,
+                                                                longitude = longitude)
+                option_obj.map_location = map_location_obj
+            
+            option_obj.save()
+            
+        service_obj.extra_details = sections
+        service_obj.save()
+        
+        return Response({'message': 'Service created successfully'})
 
 def get_services_helper(services):
     res = []
